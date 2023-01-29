ca810be65800ff1d8b5b4c4b71cb10c97942c451
raiyuvraj <yuvirai017@gmail.com>
2023-01-29 13:26:03 +0530

    [Yuvraj]-Frequency Of Words in Large Para

--- Hash_Table/Program.cs
+++ Hash_Table/Program.cs
@@ -4,13 +4,26 @@
     {
         static void Main(string[] args)
         {
-            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(19);
+            hash.Add(0, "Paranoids");
+            hash.Add(1, "are");
+            hash.Add(2, "not");
+            hash.Add(3, "paranoid");
+            hash.Add(4, "becuse");
+            hash.Add(5, "they");
+            hash.Add(6, "are");
+            hash.Add(7, "paranoid");
+            hash.Add(8, "but");
+            hash.Add(9, "because");
+            hash.Add(10, "they");
+            hash.Add(11, "keep");
+            hash.Add(12, "putting");
+            hash.Add(13, "themselves");
+            hash.Add(14, "deliberately");
+            hash.Add(15, "into");
+            hash.Add(16, "paranoid");
+            hash.Add(17, "avoidable");
+            hash.Add(18, "situations");
 
             string hash0 = hash.Get(0);
             Console.WriteLine("0th index value: " + hash0);

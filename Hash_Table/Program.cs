e6002c3e41a5a381d678483eb2cead7c67af15f6
raiyuvraj <yuvirai017@gmail.com>
2023-01-29 13:41:49 +0530

    [Yuvraj]-Remove_Words

--- Hash_Table/Program.cs
+++ Hash_Table/Program.cs
@@ -25,34 +25,17 @@
             hash.Add(17, "avoidable");
             hash.Add(18, "situations");
 
-            string hash0 = hash.Get(0);
-            Console.WriteLine("0th index value: " + hash0);
-
-            string hash1 = hash.Get(1);
-            Console.WriteLine("1st index value: " + hash1);
-
-            string hash2 = hash.Get(2);
-            Console.WriteLine("2nd index value: " + hash2);
-
-            string hash3 = hash.Get(3);
-            Console.WriteLine("3rd index value: " + hash3);
-
-            string hash4 = hash.Get(4);
-            Console.WriteLine("4th index value: " + hash4);
-
-            string hash5 = hash.Get(5);
-            Console.WriteLine("5th index value: " + hash5);
-
-            Console.WriteLine("\nFrequency of Words ==> ");
-
-            string[] arr = new string[hash.size];
             for (int i = 0; i < hash.size; i++)
             {
-                arr[i] = hash.Get(i);
+                Console.WriteLine($"{i} index value : {hash.Get(i)}");
             }
 
-            hash.FreqOfWords(arr, arr.Length);
+            hash.Remove(14);
 
+            for (int i = 0; i < hash.size; i++)
+            {
+                Console.WriteLine($"{i} index value : {hash.Get(i)}");
+            }
             Console.ReadLine();
         }
     }
